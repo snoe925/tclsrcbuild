85d7e8ccb827f845a9f80f9008cee058d22656d9
Shannon Noe <shannon.noe@flightaware.com>
2017-12-06 16:11:27 -0600

    Split OS Makefiles

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
+	echo "Build on OS X"
+    	cd src; make -f Makefile.osx
+else
+	echo "Build on Linux"
+	cd src; make -f Makefile.linux
+endif
